c311f9b23e323f258dbf08de990ab06957bc28ee
by_Onyx <arbit.ia@>
2024-07-01 19:36:28 +0500

    Добавил cors

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import api_router
 
@@ -7,7 +7,22 @@ app = FastAPI()
 
 app.include_router(api_router, prefix="/api")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def main():
+    return {"message": "Hello World"}
+
+
 if __name__ == '__main__':
+    import uvicorn
     uvicorn.run(
         app,
         host="127.0.0.1",
