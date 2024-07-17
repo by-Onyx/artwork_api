21bc66bb3b39fe75c56fb258489295453c1e8b1b
by_Onyx <arbit.ia@>
2024-07-18 02:23:41 +0500

    Изменил фото и вернул create_image

--- app/api/endpoints/image.py
+++ app/api/endpoints/image.py
@@ -11,12 +11,12 @@ from app.service import image_service
 router = APIRouter(prefix="/image", tags=["image"])
 
 
-@router.post("/", response_model=bool)
+@router.post("/", response_model=Image)
 async def create_image(file: UploadFile, category_id: int = Form(int),
                        creation_year: int = Form(int),
                        db: Session = Depends(get_db)):
     image = image_service.add_text_to_image(image=file)
-    # if image is None:
-    #     raise HTTPException(status_code=404, detail='Image not created')
-    # artwork_image = image_crud.create(db, name=file.filename, category_id=category_id, creation_year=creation_year)
-    return True
+    if image is None:
+        raise HTTPException(status_code=404, detail='Image not created')
+    artwork_image = image_crud.create(db, name=file.filename, category_id=category_id, creation_year=creation_year)
+    return artwork_image
